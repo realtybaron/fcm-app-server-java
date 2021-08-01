afcbe5cc6fde30e0b3cb6d92666f85c2a75fac35
Marc Dugger <mdugger@gmail.com>
2021-08-01 16:58:06 -0500

    fixed device registration

--- src/main/java/com/socotech/fcm/http/Sender.java
+++ src/main/java/com/socotech/fcm/http/Sender.java
@@ -16,6 +16,9 @@
 package com.socotech.fcm.http;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.InstanceCreator;
@@ -26,8 +29,6 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Helper class to send messages to the GCM service using an API Key.
@@ -35,37 +36,58 @@ import java.util.logging.Logger;
 public class Sender {
 
     private String accessToken;
+    private GoogleCredential credential;
 
     private final Gson gson;
     private final String url;
     private final Random random;
-    private final GoogleCredential credential;
 
     /**
      * Default constructor.
      *
      * @param projectId project ID
+     * @throws IOException i/o exception
      */
     public Sender(String projectId) throws IOException {
-        this(projectId, Thread.currentThread().getContextClassLoader());
-    }
-
-    /**
-     * Default constructor.
-     *
-     * @param projectId   project ID
-     * @param classLoader class loader used to find "service-account.json"
-     * @throws IOException
-     */
-    public Sender(String projectId, ClassLoader classLoader) throws IOException {
         this.url = String.format("https://fcm.googleapis.com/v1/projects/%s/messages:send", projectId);
         this.gson = new GsonBuilder().registerTypeAdapter(Message.class, (InstanceCreator<Message>) type -> new Message.Builder().build()).create();
         this.random = new Random();
-        String scope = "https://www.googleapis.com/auth/firebase.messaging";
-        InputStream stream = classLoader.getResourceAsStream("service-account.json");
-        this.credential = GoogleCredential.fromStream(stream).createScoped(Collections.singletonList(scope));
-        this.credential.refreshToken();
-        this.accessToken = credential.getAccessToken();
+    }
+
+    /**
+     * Configure sender using JSON
+     *
+     * @param serviceJson service JSON
+     * @return this sender
+     * @throws IOException i/o exception
+     */
+    public Sender configure(String serviceJson) throws IOException {
+        if (credential == null) {
+            String scope = "https://www.googleapis.com/auth/firebase.messaging";
+            InputStream stream = new ByteArrayInputStream(serviceJson.getBytes());
+            this.credential = GoogleCredential.fromStream(stream).createScoped(Collections.singletonList(scope));
+            this.credential.refreshToken();
+            this.accessToken = credential.getAccessToken();
+        }
+        return this;
+    }
+
+    /**
+     * Configure sender using service file
+     *
+     * @param classLoader class loader
+     * @return this sender
+     * @throws IOException i/o exception
+     */
+    public Sender configure(ClassLoader classLoader) throws IOException {
+        if (credential == null) {
+            String scope = "https://www.googleapis.com/auth/firebase.messaging";
+            InputStream stream = classLoader.getResourceAsStream("service-account.json");
+            this.credential = GoogleCredential.fromStream(stream).createScoped(Collections.singletonList(scope));
+            this.credential.refreshToken();
+            this.accessToken = credential.getAccessToken();
+        }
+        return this;
     }
 
     /**
@@ -133,25 +155,19 @@ public class Sender {
             status = conn.getResponseCode();
             switch (status) {
                 case 200:
-                    responseBody = getAndClose(conn.getInputStream());
-                    LOGGER.log(Level.INFO, "JSON response: " + responseBody);
-                    return responseBody;
+                    return getAndClose(conn.getInputStream());
                 case 401:
                     if (this.credential.refreshToken()) {
                         this.accessToken = credential.getAccessToken();
                         return null; // force retry
-                    } else {
-                        LOGGER.log(Level.WARNING, "Refreshing access token failed");
                     }
                 default:
                     responseBody = getAndClose(conn.getErrorStream());
-                    LOGGER.log(Level.WARNING, "JSON error response: " + responseBody);
                     throw new InvalidRequestException(status, responseBody);
             }
         } catch (InvalidRequestException e) {
             throw e;
         } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "IOException posting to FCM", e);
             return null;
         } finally {
             if (conn != null) {
@@ -177,11 +193,6 @@ public class Sender {
         if (url == null || contentType == null || body == null) {
             throw new IllegalArgumentException("arguments cannot be null");
         }
-        if (!url.startsWith("https://")) {
-            LOGGER.log(Level.WARNING, "URL does not use https: " + url);
-        }
-        LOGGER.log(Level.INFO, "Send to " + url);
-        LOGGER.log(Level.INFO, "POST body: " + body);
         byte[] bytes = body.getBytes(StandardCharsets.UTF_8);
         HttpURLConnection conn = getConnection(url);
         conn.setDoOutput(true);
@@ -247,7 +258,6 @@ public class Sender {
         }
     }
 
-    private static final String UTF8 = "UTF-8";
     /**
      * Maximum delay before a retry.
      */
@@ -257,7 +267,17 @@ public class Sender {
      */
     private static final int BACKOFF_INITIAL_DELAY = 1000;
     /**
-     * Logger
+     * Cache of app senders
      */
-    private static final Logger LOGGER = Logger.getLogger(Sender.class.getName());
+    public static final LoadingCache<String, Sender> cache = CacheBuilder.newBuilder().build(new CacheLoader<String, Sender>() {
+        @Override
+        public Sender load(String projectId) {
+            try {
+                return new Sender(projectId);
+            } catch (Exception e) {
+                return null;
+            }
+        }
+    });
+
 }
